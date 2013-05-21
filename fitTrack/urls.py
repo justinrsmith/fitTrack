bf38b457ef9ccd95e523cfc7ca4555434cc8a138
unknown <justinsmith@GX7800010.sd54.k12.il.us>
2013-05-21 12:05:31 -0500

    Able to collect and post data

--- fitTrack/urls.py
+++ fitTrack/urls.py
@@ -4,4 +4,4 @@ import views as v
 url = app.add_url_rule
 
 url('/', 'home', v.home)
-url('/track', 'track', v.track)
+url('/track', 'track', v.track, methods = ['POST', 'GET'])
--- fitTrack/views.py
+++ fitTrack/views.py
@@ -4,10 +4,18 @@ import models as m
 
 def home():
     """Home page"""
+    #return "hi"
     return render_template('home.html')
 
 def track():
-    """A list of all employees"""
+    """Gather exercise details, insert into DB"""
+   
+    if request.method == 'POST':
+    	print [request.form['exercise']]
+    	main = m.main(request.form['exercise'])
+    	m.db.session.add(main)
+    	m.db.session.commit()
+
     exercise = m.main.query.all()
 
     return render_template('track.html',exercise=exercise)
